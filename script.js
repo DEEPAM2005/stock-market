d32b044f7e373a043d44f097795972bffadd615e
DEEPAM2005 <154005122+DEEPAM2005@users.noreply.github.com>
2024-05-09 15:07:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let scrollContainer = document.querySelector(".gallery");
+let backbtn = document.getElementById("backbtn");
+let nextbtn = document.getElementById("nextbtn");
+
+scrollContainer.addEventListener("wheel", (evt)=>{
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehaviour = "auto"
+});
+
+nextbtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehaviour = "smooth"
+    scrollContainer.scrollLeft += 1000
+})
+backbtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehaviour = "smooth"
+    scrollContainer.scrollLeft -= 1000
+})
